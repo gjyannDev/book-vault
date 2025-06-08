e37b7b8c73709257c9ca7e83545f63c666c64856
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-06-08 22:28:13 +0800

    build: finish setting up the shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-playfair-">BookVault</h1>
+      <h1 className="text-5xl">BookVault</h1>
+
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
     </>
   );
 }
