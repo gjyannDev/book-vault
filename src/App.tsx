4c3e828e7dba33188d70627544c72243b9e808b4
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-06-08 23:05:42 +0800

    build: set up RTL for testing react

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 function App() {
   return (
     <>
       <h1 className="text-5xl">BookVault</h1>
-
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
     </>
   );
 }
