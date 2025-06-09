7c4582f1d8341e2fb6092512d66da824f936914c
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-06-09 22:30:55 +0800

    chore: set up the validation and form for the search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,77 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import heroImage from "./assets/images/heroSectionImage.png";
 import NavBar from "./components/layout/NavBar";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "./components/ui/form";
+import { Input } from "./components/ui/input";
+
+const FormSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Title must be atleast 5 characters." })
+    .regex(/^[A-Za-z\s\-']+$/, {
+      message: "Only letters, spaces, hyphens and apostrophes are allowed.",
+    }),
+});
 
 function App() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  function handleSubmit(data: z.infer<typeof FormSchema>) {
+    console.log("data: ", data);
+  }
+
   return (
     <>
       <NavBar />
-      <div className="container mx-auto px-4 sm:px-8 lg:px-14"></div>
+      <div className="container mx-auto px-4 sm:px-8 lg:px-14">
+        <div className="flex flex-col justify-center w-full">
+          <div className="">
+            <h1 className="text-5xl font-playfair-bold text-[var(--primary-text)]">
+              Your Gateway to a Smarter Shelf.
+            </h1>
+            <p className="font-lora-regular text-[var(--primary-text)]">
+              One bookstore. Infinite journeys. Build your collection with the
+              best reads around.
+            </p>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSubmit)} className="">
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          placeholder="Search book title or author."
+                          {...field}
+                          className="placeholder:text-[var(--primary-text)] rounded-sm focus:ring-2 focus:ring-[#b17457]"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+          </div>
+          <div>
+            <img src={heroImage} alt="hero image" className="w-[356px]" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
