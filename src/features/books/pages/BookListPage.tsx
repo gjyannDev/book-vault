214f15ce3869d29f2a6945e16a575c24bd692a19
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-06-12 23:13:16 +0800

    chore: added some things and remove the unnecessary one

---
+++ src/features/books/pages/BookListPage.tsx
@@ -0,0 +1,12 @@
+export default function BookListPage() {
+  return (
+    <div className="">
+      <div className="">
+
+      </div>
+
+      <div className=""></div>
+
+    </div>
+  );
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,15 @@
 import App from "./App";
+// import BookListPage from "./features/books/pages/BookListPage";
 
 const routes = [
   {
     path: "/",
     element: <App />,
   },
+  // {
+  //   path: "books",
+  //   element:
+  // }
 ];
 
 export default routes;
