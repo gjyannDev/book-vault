e89ce0c8b5bebd9069d83708fc0c1451b0bc1b26
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-06-16 10:50:56 +0800

    chore: add responsive design on xl ratio

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -4,19 +4,22 @@ import visionImage from "../assets/images/visionImage.png";
 
 export default function AboutPage() {
   return (
-    <div className="text-[var(--primary-text)] mb-14 lg:mb-20">
-      <h1 className="font-playfair-bold text-3xl py-6 md:text-4xl md:py-8">
+    <div className="text-[var(--primary-text)] mb-14 lg:mb-20 xl:mb-28">
+      <h1 className="font-playfair-bold text-3xl py-6 md:text-4xl md:py-8 xl:text-5xl xl:py-12">
         ABOUT US
       </h1>
       <div className="flex flex-col gap-16 lg:gap-24">
         <div className="flex flex-col gap-4 lg:gap-6">
-          <img src={aboutUsImage} className="" />
+          <img
+            src={aboutUsImage}
+            className="mx-auto md:w-[732px] lg:w-[856px] xl:w-[1024px]"
+          />
           <div className="flex flex-col gap-4">
-            <h1 className="font-playfair-bold text-xl md:text-2xl md:max-w-xl lg:text-3xl lg:max-w-2xl">
+            <h1 className="font-playfair-bold text-xl md:text-2xl md:max-w-xl lg:text-3xl lg:max-w-2xl xl:text-4xl xl:max-w-4xl xl:leading-[1.4]">
               Your trusted destination for discovering, buying, and falling in
               love with books.
             </h1>
-            <p className="font-lora-regular text-sm leading-[1.5] md:text-md md:max-w-xl lg:max-w-2xl lg:leading-[1.7]">
+            <p className="font-lora-regular text-sm leading-[1.5] md:text-md md:max-w-xl lg:max-w-2xl lg:leading-[1.7] xl:text-lg xl:max-w-3xl">
               At BookVault, we believe books are more than just pages — they are
               gateways to knowledge, imagination, and inspiration. Founded with
               a passion for reading and a mission to make books more accessible,
@@ -27,23 +30,27 @@ export default function AboutPage() {
             </p>
           </div>
         </div>
-        <div className="flex flex-col gap-8 md:gap-12">
-          <div className="flex flex-col gap-2 md:flex-row md:gap-6">
+        <div className="flex flex-col gap-8 md:gap-12 xl:gap-24">
+          <div className="flex flex-col gap-2 md:flex-row md:gap-6 xl:gap-12">
             <img src={visionImage} className="md:w-1/2 md:block" />
             <div className="flex flex-col md:w-1/2 md:gap-2">
-              <h1 className="font-playfair-bold text-xl md:text-2xl lg:text-3xl">Our Vision</h1>
-              <p className="font-lora-regular text-sm lg:text-md lg:leading-[1.5]">
+              <h1 className="font-playfair-bold text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+                Our Vision
+              </h1>
+              <p className="font-lora-regular text-sm lg:text-md lg:leading-[1.5] xl:text-lg">
                 We envision a future where books are not a luxury but a
                 lifestyle, empowering people to learn, grow, and connect through
                 the power of stories and information.
               </p>
             </div>
           </div>
-          <div className="flex flex-col gap-2 md:flex-row-reverse">
+          <div className="flex flex-col gap-2 md:flex-row-reverse xl:gap-x-12 ">
             <img src={missionImage} className="md:w-1/2 md:block" />
             <div className="flex flex-col md:w-1/2 md:gap-2">
-              <h1 className="font-playfair-bold text-xl md:text-2xl lg:text-3xl">Our Mission</h1>
-              <p className="font-lora-regular text-sm lg:text-md lg-leading-[1.5]">
+              <h1 className="font-playfair-bold text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+                Our Mission
+              </h1>
+              <p className="font-lora-regular text-sm lg:text-md lg-leading-[1.5] xl:text-lg">
                 We envision a future where books are not a luxury but a
                 lifestyle, empowering people to learn, grow, and connect through
                 the power of stories and information.
