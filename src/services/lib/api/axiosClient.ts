a86082e1d07e10adc8e260e9f6d88f38c4cb4e33
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-06-19 23:46:35 +0800

    chore: created axios client for axios things

---
+++ src/services/lib/api/axiosClient.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://www.googleapis.com/books/v1/volumes",
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
