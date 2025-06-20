599bf40f74ee455a49c6ed00149653e62eac08e3
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-06-20 14:31:11 +0800

    chore: created axios client and also add env file

--- src/services/lib/api/axiosClient.ts
+++ src/services/lib/api/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://www.googleapis.com/books/v1/volumes",
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     "Content-Type": "application/json",
   },
