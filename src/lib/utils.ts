c1a125413735322e3d95f5c241fe919c0e56ec27
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-06-25 23:47:16 +0800

    chore: add new utils for accessing the min and max of a price

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import type { MinMaxPriceRange } from "@/types/bookTypes";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getMinMaxPriceRange(value: string): MinMaxPriceRange {
+  // if (value === "all") return {};
+
+  const [minStr, maxStr] = value.split("-");
+  const min = parseInt(minStr, 10);
+  const max = parseInt(maxStr, 10);
+
+  return {min, max};
 }
