e748288029f420f3e838ae785b3cdff167f39b0c
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-06-26 23:08:01 +0800

    chore: created new utils function for sorting word in asc and desc

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import type { MinMaxPriceRange } from "@/types/bookTypes";
+import type { BookInfo, MinMaxPriceRange } from "@/types/bookTypes";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -13,5 +13,19 @@ export function getMinMaxPriceRange(value: string): MinMaxPriceRange {
   const min = parseInt(minStr, 10);
   const max = parseInt(maxStr, 10);
 
-  return {min, max};
+  return { min, max };
+}
+
+export function sortWordAlphabetically(
+  type: string,
+  data: BookInfo[],
+  setBooks: (books: BookInfo[]) => void
+) {
+  const sorted_books = [...data].sort((a, b) => {
+    return type === "asc"
+      ? a.title.localeCompare(b.title)
+      : b.title.localeCompare(a.title);
+  });
+
+  setBooks(sorted_books);
 }
