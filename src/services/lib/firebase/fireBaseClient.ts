f9353e848c7fe8f280fe780f066b26eb286a1290
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-06-30 22:58:19 +0800

    build: initial set up for firebase with using get auth and get fire store

--- src/services/lib/firebase/fireBaseClient.ts
+++ src/services/lib/firebase/fireBaseClient.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 export const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -10,3 +12,8 @@ export const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export default app;
