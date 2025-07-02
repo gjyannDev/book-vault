42c12bea551e2660917e689cdeec2bee8fb1f840
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-07-02 09:36:22 +0800

    chore: created new class name for error hint text

--- src/components/shared/classNames.ts
+++ src/components/shared/classNames.ts
@@ -4,3 +4,7 @@ export const headerBase = clsx(
   "font-playfair-bold text-3xl text-[var(--primary-text)]",
   "lg:text-4xl xl:text-5xl"
 );
+
+export const errorHintText = clsx(
+  "font-lora-regular text-sm text-destructive"
+)
