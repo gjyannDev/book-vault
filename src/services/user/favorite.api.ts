fe31347833bc54486dedc10eaf3f96ec7783a330
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-07-08 09:26:48 +0800

    chore: created api for getting all favorite books

--- src/services/user/favorite.api.ts
+++ src/services/user/favorite.api.ts
@@ -1,5 +1,5 @@
 import type { AddFavoriteProps } from "@/types/bookTypes";
-import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { arrayUnion, doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "../lib/firebase/fireBaseClient";
 
 export async function addFavorite(params: AddFavoriteProps) {
@@ -8,6 +8,18 @@ export async function addFavorite(params: AddFavoriteProps) {
     const res = await updateDoc(ref, { favorites: arrayUnion(params) });
 
     return res;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getFavorites(userId: string) {
+  try {
+    const ref = doc(db, "users", userId);
+    const res = await getDoc(ref);
+    const data = res.data();
+
+    return data?.favorites || [];
   } catch (error) {
     console.error(error);
   }
