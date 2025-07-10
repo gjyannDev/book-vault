543f426345c007aedc18c4261d586a9b0aab6bcc
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-07-10 14:00:13 +0800

    chore: able to get cart data in a sub collection

--- src/services/user/cart.api.ts
+++ src/services/user/cart.api.ts
@@ -1,22 +1,16 @@
-import type { CartProps } from "@/types/bookTypes";
-import { arrayUnion, getDoc, setDoc } from "firebase/firestore";
-import { getUserRef } from "../lib/firebase/fireBaseClient";
+import type { BookInfo, CartProps, UpdateCartProps } from "@/types/bookTypes";
+import { getDocs, setDoc } from "firebase/firestore";
+import { getCartRefCol, getCartRefDoc } from "../lib/firebase/fireBaseClient";
 
 export async function addCart(params: CartProps) {
   try {
-    const ref = getUserRef();
+    const ref = getCartRefDoc(params.id);
 
     if (!ref) {
       throw new Error("User is not logged in.");
     }
 
-    const res = await setDoc(
-      ref,
-      {
-        cart: arrayUnion(params),
-      },
-      { merge: true }
-    );
+    const res = await setDoc(ref, params, { merge: true });
 
     return res;
   } catch (error) {
@@ -24,16 +18,40 @@ export async function addCart(params: CartProps) {
   }
 }
 
-export async function getCartsData() {
+export async function getCartsData(): Promise<BookInfo[]> {
   try {
-    const ref = getUserRef();
+    const ref = getCartRefCol();
     if (!ref) {
       throw new Error("User is not logged in.");
     }
-    const res = await getDoc(ref);
-    const data = res.data();
+    const res = await getDocs(ref);
 
-    return data?.cart || [];
+    const cartItems: BookInfo[] = res.docs.map((doc) => ({
+      id: doc.id,
+      ...(doc.data() as Omit<BookInfo, "id">),
+    }));
+
+    return cartItems;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function updateCartData(params: UpdateCartProps) {
+  try {
+    const ref = getCartRefDoc(params.bookId);
+    console.log("Ref path:", ref.path);
+    const res = await setDoc(
+      ref,
+      {
+        quantity: params.quantity,
+        total: params.total,
+      },
+      { merge: true }
+    );
+
+    return res;
   } catch (error) {
     console.error(error);
   }
